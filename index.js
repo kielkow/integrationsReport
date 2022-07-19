2ee8e07c10f789cac46e85661b17fa557f51dcf7
kielkow <matheuskiel@fiorifer.com.br>
2022-07-19 00:49:22 -0300

    use dotenv to set token

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv/config');
+
 const fs = require('fs');
 const axios = require('axios');
 const lodash = require('lodash');
@@ -20,7 +22,7 @@ const report = async () => {
                     _sort_displayName: 'asc'
                 },
                 headers: {
-                    authorization: `Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg5MDJDMkZEQ0IxNjM2NDk1ODJBQjFCQzNBOTU5MDJDNDhCNzhENjMiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJpUUxDX2NzV05rbFlLckc4T3BXUUxFaTNqV00ifQ.eyJuYmYiOjE2NTgxNzkwNzAsImV4cCI6MTY1ODIwNzg3MCwiaXNzIjoiaHR0cHM6Ly9pZGVudGl0eS5saW5rYXBpLmNvbS5iciIsImF1ZCI6WyJodHRwczovL2lkZW50aXR5LmxpbmthcGkuY29tLmJyL3Jlc291cmNlcyIsImxrcF9hcGlwb3J0YWwiLCJsa3BfZGlzdHJpYnV0aW9uIiwibGtwaWQ0Il0sImNsaWVudF9pZCI6ImxrcF9kZXZlbG9wZXJfcG9ydGFsXzEiLCJzdWIiOiI2OTY5ZDA3Ni0yOWNhLTQyMjEtOTdkZC0yM2QyYWE2Y2QyMmUiLCJhdXRoX3RpbWUiOjE2NTcyOTE3ODAsImlkcCI6ImxvY2FsIiwiZ2l2ZW5fbmFtZSI6Ik1hdGhldXMiLCJtaWRkbGVfbmFtZSI6IktpZWxrb3dza2kiLCJuaWNrbmFtZSI6Ik1hdGhldXMiLCJwaWN0dXJlIjoicGljdHVyZSIsIm9mZmljZSI6IiIsImV4dGVybmFsaWQiOiI1YTBlMTY4OWY4ODQzODE1ZjRmNWU5YzkiLCJleHRlcm5hbHR5cGUiOiJzZWxsZXIiLCJyb2xlIjoibGtwLmJ1c2luZXNzIiwibmFtZSI6Im1hdGhldXMua2llbGtvd3NraUBsaW5rYXBpLmNvbS5iciIsImVtYWlsIjoibWF0aGV1cy5raWVsa293c2tpQGxpbmthcGkuY29tLmJyIiwic2NvcGUiOlsiZW1haWwiLCJsa3BkZWZhdWx0Iiwib3BlbmlkIiwicHJvZmlsZSIsImxrcF9hcGlwb3J0YWwiLCJsa3BfZGlzdC5yZWFkIiwibGtwX2Rpc3Qud3JpdGUiLCJsa3BpZDQucmVhZCIsIm9mZmxpbmVfYWNjZXNzIl0sImFtciI6WyJwd2QiXX0.g7dBoYG64ZqQNc53U1a-Ist_w8ttUR7CFW4H2s0pYweBwLd-Gin3LIYfbCmCt-dXHTjfdgIpkL9DgI-Hg_OAJxCtxVSycNb1jbt5GYM-snEXQAaFIguC-cjSakBnHRsdSOdbGpXkYTlHF_WXztDmCSOQuXAu4C2YpJnOwfJsqqGuEaAwMde_kPQop3kmIVDIUUfVwGwj6CCdZzrVL0bEVDTNmFOdS6Ocmgzhm_C0QuXCOPhUTeN2qqQEDQxq6ip_h3-cO0S5UBtvS19ciZ7guJ66VY5TNKGM8635l8Ta9hAV8fqd8Ne2vTz9mSvJcrVKKYq76PE9XSnVIajTJmYk-A`
+                    authorization: `Bearer ${process.env.TOKEN}`
                 },
             });
 
@@ -61,7 +63,7 @@ const report = async () => {
                     name: groupedIntegrationsByProjects[project][0].project_name,
                     integrations: groupedIntegrationsByProjects[project].map(integration => ({
                         job_name: integration.job_name,
-                        automation_file_name: integration. automation_file_name
+                        automation_file_name: integration.automation_file_name
                     }))
                 })
             }
